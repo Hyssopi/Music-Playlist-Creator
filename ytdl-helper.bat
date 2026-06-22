b5a1c72fbb165261a1e019cf465b8d8e0c362e6f
Hyssopi <43843070+Hyssopi@users.noreply.github.com>
2026-06-22 01:18:16 -0700

    Update

--- ytdl-helper.bat
+++ ytdl-helper.bat
@@ -1,3 +1,3 @@
-yt-dlp_win\yt-dlp.exe --extract-audio --embed-thumbnail --embed-chapters --min-sleep-interval 20 --max-sleep-interval 25 -o "Downloaded/%%(id)s.%%(ext)s" --batch-file "ytdl-helper-links.txt"
+yt-dlp_win\yt-dlp.exe --ffmpeg-location "ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe" --extract-audio --embed-thumbnail --embed-chapters --min-sleep-interval 20 --max-sleep-interval 25 -o "Downloaded/%%(id)s.%%(ext)s" --batch-file "ytdl-helper-links.txt"
 
 PAUSE
